2a46fdec6ea8a75a2a9eb70790ac673567740c72
Lane Kolbly <lane@rscheme.org>
2020-03-02 20:40:27 -0600

    Remove rust memcpy
    The builtin rust memory management routines conflict with the ESP-provided ones. If the built binary uses the ones generated by rust, Weird Things will happen (no crashes, per se, but WiFi will mysteriously fail).
    
    Also add includes for esp32-sys

--- esp32-sys/src/bindings.h
+++ esp32-sys/src/bindings.h
@@ -1,2 +1,15 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/portmacro.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
 #include <driver/uart.h>
+#include <driver/gpio.h>
+#include <esp_wifi.h>
+#include <esp_event_loop.h>
+#include <nvs_flash.h>
+#include <mqtt_client.h>
+#include <esp_log.h>
+#include <esp_netif.h>
 
+// For our own custom GPIO driver
+#include <esp8266/gpio_struct.h>
